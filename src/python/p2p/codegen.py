6fc772b1f8efea9ace430d7b2c67fe481a9bcf28
Alonso Rodriguez <alonso.rodriguez@udc.es>
2022-09-27 14:16:54 +0200

    Fix more : and add initial OpenMP support for P2P codegen

--- src/python/p2p/codegen.py
+++ src/python/p2p/codegen.py
@@ -1,7 +1,7 @@
 def fn_algebra_ops(args):
     idx, local_layer_lud, MAP_ALGORITHM = args
 
-    local_algebra_ops = [f"    fprintf(stderr, \"*** POINT-TO-POINT MATMUL %s x %s ***\\n\", \"layer{idx-1}\", \"layer{idx}\");"]
+    local_algebra_ops = [f"#ifdef DEBUG_VERBOSE\n#pragma omp master\n    fprintf(stderr, \"*** POINT-TO-POINT MATMUL %s x %s ***\\n\", \"layer{idx-1}\", \"layer{idx}\");\n#endif"]
 
     local_p2p_ops = []
     for x,y in local_layer_lud.keys():
@@ -9,13 +9,14 @@ def fn_algebra_ops(args):
 
     # weird indentation to make it look pretty (nothing else :)
     local_algebra_ops.append(f"""
+#pragma omp for
     for(int k = 0; k < input_dim; k++){{
         temp_input = layer{idx-1}_out + k*LAYER{idx-1}_SIZE;
         temp_output = layer{idx}_out + k*LAYER{idx}_SIZE;\n\n{"".join(local_p2p_ops)}    }}
-    """)
+""")
 
     # Una optimización muy sencilla es meter el free de layer{idx-1} después del sgemm, pero debido a que esto es una poc, no voy a complicarme ahora mismo, a no ser que sea necesario. 
-    local_algebra_ops.append(f"fprintf(stderr, \"*** Map and Bias %s(T) with %s function ***\\n\", \"layer{idx}_out\", \"{MAP_ALGORITHM}\");\n")
-    local_algebra_ops.append(f"    map_and_bias__fp32(layer{idx}_out, layer{idx}_bias, input_dim, LAYER{idx}_SIZE, 'N', {MAP_ALGORITHM});\n\n")
+    local_algebra_ops.append(f"#ifdef DEBUG_VERBOSE\n#pragma omp master\nfprintf(stderr, \"*** Map and Bias %s(T) with %s function ***\\n\", \"layer{idx}_out\", \"{MAP_ALGORITHM}\");\n#endif\n")
+    local_algebra_ops.append(f"#pragma omp single\n    map_and_bias__fp32(layer{idx}_out, layer{idx}_bias, input_dim, LAYER{idx}_SIZE, 'N', {MAP_ALGORITHM});\n\n")
 
     return "".join(local_algebra_ops)
