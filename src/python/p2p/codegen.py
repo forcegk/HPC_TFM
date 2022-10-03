c5984f0974b226c7fd48c34e3930675513c64bfd
Alonso Rodriguez <alonso.rodriguez@udc.es>
2022-10-03 14:21:03 +0200

    Backport colors and #ifdef VERBOSE, extend tagging to errors, and fix #define placements on p2p backend

--- src/python/p2p/codegen.py
+++ src/python/p2p/codegen.py
@@ -1,7 +1,7 @@
 def fn_algebra_ops(args):
     idx, local_layer_lud, MAP_ALGORITHM = args
 
-    local_algebra_ops = [f"#ifdef VERBOSE\n#pragma omp master\n{{\n    fprintf(stderr, \"*** [\"BOLDGREEN\"TIME\"RESET\"] POINT-TO-POINT MATMUL %s x %s \", \"layer{idx-1}\", \"layer{idx}\");\n    clock_gettime(CLOCK_MONOTONIC_RAW, &time_start);\n}}\n#endif"]
+    local_algebra_ops = [f"#pragma omp master\n{{\n#ifdef VERBOSE\n    fprintf(stderr, \"*** [\"BOLDGREEN\"TIME\"RESET\"] POINT-TO-POINT MATMUL %s x %s \", \"layer{idx-1}\", \"layer{idx}\");\n#endif\n    clock_gettime(CLOCK_MONOTONIC_RAW, &time_start);\n}}"]
 
     local_p2p_ops = []
     for x,y in local_layer_lud.keys():
@@ -16,8 +16,8 @@ def fn_algebra_ops(args):
 """)
 
     # Una optimización muy sencilla es meter el free de layer{idx-1} después del sgemm, pero debido a que esto es una poc, no voy a complicarme ahora mismo, a no ser que sea necesario. 
-    local_algebra_ops.append(f"#ifdef VERBOSE\n#pragma omp master\n{{\n    clock_gettime(CLOCK_MONOTONIC_RAW, &time_end);\n    elapsed_us = (time_end.tv_sec - time_start.tv_sec) * 1000000 + (time_end.tv_nsec - time_start.tv_nsec) / 1000;\n    delta_gemm_us += elapsed_us;\n    fprintf(stderr, \"took %d µs ***\\n\", elapsed_us);\n    fprintf(stderr, \"*** [\"BOLDGREEN\"TIME\"RESET\"] Map and Bias %s(T) with %s function \", \"layer{idx}_out\", \"{MAP_ALGORITHM}\");\n    clock_gettime(CLOCK_MONOTONIC_RAW, &time_start);\n}}\n#endif\n")
+    local_algebra_ops.append(f"#pragma omp master\n{{\n    clock_gettime(CLOCK_MONOTONIC_RAW, &time_end);\n    elapsed_us = (time_end.tv_sec - time_start.tv_sec) * 1000000 + (time_end.tv_nsec - time_start.tv_nsec) / 1000;\n    delta_gemm_us += elapsed_us;\n#ifdef VERBOSE\n    fprintf(stderr, \"took %d µs ***\\n\", elapsed_us);\n    fprintf(stderr, \"*** [\"BOLDGREEN\"TIME\"RESET\"] Map and Bias %s(T) with %s function \", \"layer{idx}_out\", \"{MAP_ALGORITHM}\");\n#endif\n    clock_gettime(CLOCK_MONOTONIC_RAW, &time_start);\n}}\n")
     local_algebra_ops.append(f"    map_and_bias__fp32(layer{idx}_out+start*LAYER{idx}_SIZE, layer{idx}_bias, end-start, LAYER{idx}_SIZE, 'N', {MAP_ALGORITHM});\n#pragma omp barrier\n")
-    local_algebra_ops.append(f"#ifdef VERBOSE\n#pragma omp master\n{{\n    clock_gettime(CLOCK_MONOTONIC_RAW, &time_end);\n    elapsed_us = (time_end.tv_sec - time_start.tv_sec) * 1000000 + (time_end.tv_nsec - time_start.tv_nsec) / 1000;\n    delta_map_bias_us += elapsed_us;\n    fprintf(stderr, \"took %d µs ***\\n\", elapsed_us);\n}}\n#endif\n\n")
+    local_algebra_ops.append(f"#pragma omp master\n{{\n    clock_gettime(CLOCK_MONOTONIC_RAW, &time_end);\n    elapsed_us = (time_end.tv_sec - time_start.tv_sec) * 1000000 + (time_end.tv_nsec - time_start.tv_nsec) / 1000;\n    delta_map_bias_us += elapsed_us;\n#ifdef VERBOSE\n    fprintf(stderr, \"took %d µs ***\\n\", elapsed_us);\n#endif\n}}\n\n")
 
     return "".join(local_algebra_ops)
